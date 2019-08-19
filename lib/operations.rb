c1f3c1326e3880535128fa3f5a9694a378baeab3
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-19 20:48:21 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
-def unsafe?(speed)
-
-end
+age = 1
+ 
+if age < 2
+    "baby"
+else
+    "not a baby"
+end 
 
 
 
 def not_safe?(speed)
-	
+	 (speed < 40 || speed > 60 ? true : false)
+	end
 end
-	
-
-
